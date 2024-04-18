073387727edf1cb7259abe963104b4278d7bbc91
Hassan Alharbi <eng.hassanalharbi@gmail.com>
2024-04-18 20:05:00 +0300

    salman is the goat

--- src/main.c
+++ src/main.c
@@ -2,18 +2,18 @@
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 640;
+    const int screenHeight = 320;
 
     InitWindow(screenWidth, screenHeight, "Test");
 
-    SetTargetFPS(60);
+    SetTargetFPS(144);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
             DrawText("Wow, this is a test", 300, 200, 20, LIGHTGRAY);
 
